c2cdef6f1bf9938cc11697b79d400e990d3a99f3
Rogim <rogertti@gmail.com>
2023-10-19 20:56:31 -0300

    v 1.4

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $investimento = new Investimento($db);
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-    <!-- Select Picker -->
+    <!-- Date Picker -->
     <link rel="stylesheet" href="plugins/datepicker/css/datepicker.min.css">
     <!-- Select Picker -->
     <link rel="stylesheet" href="plugins/bootstrap-select-1.13.14/css/bootstrap-select.min.css">
